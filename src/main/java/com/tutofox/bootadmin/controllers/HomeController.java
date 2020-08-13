42c53cafd54d42b9078a4ea39375fd2e30adec00
Jirayut Srisawangwong <jirayut_ssww@hotmail.com>
2020-08-13 22:56:34 +0700

    add Home Controller

---
+++ src/main/java/com/tutofox/bootadmin/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.tutofox.bootadmin.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	@RequestMapping(value= {"/home"}, method=RequestMethod.GET)
+	 public ModelAndView home() {
+		  ModelAndView model = new ModelAndView();  
+		  model.setViewName("Home");
+		  return model;
+	 }	 
+}
