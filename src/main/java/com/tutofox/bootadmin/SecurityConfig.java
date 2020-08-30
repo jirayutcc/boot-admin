c5bb90bd5b9b9d88b4f58262b09773a4b2eb988a
Jirayut Srisawangwong <jirayut_ssww@hotmail.com>
2020-08-30 13:59:44 +0700

    Security Config

---
+++ src/main/java/com/tutofox/bootadmin/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.tutofox.bootadmin;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	protected void configure(HttpSecurity http) throws Exception {
+		 http.authorizeRequests()
+		 .antMatchers("/public").permitAll()
+		 .antMatchers("/user").hasRole("ADMIN")
+		 .antMatchers("/api/**").hasRole("USER")
+		 .and().csrf().disable()
+		 .logout()
+		 .and()
+		 .formLogin();
+	}
+	
+}
