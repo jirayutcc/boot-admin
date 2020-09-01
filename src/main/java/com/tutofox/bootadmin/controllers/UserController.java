74069e682d0d282504df7ca4b8a5e8547d58d149
Jirayut Srisawangwong <jirayut_ssww@hotmail.com>
2020-09-01 15:15:39 +0700

    update user controller for create

--- src/main/java/com/tutofox/bootadmin/controllers/UserController.java
+++ src/main/java/com/tutofox/bootadmin/controllers/UserController.java
@@ -4,7 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +26,18 @@ public class UserController {
 	public List<User> test() {
 		return userRepository.findAll();
 	}
+	
+	@PostMapping(value="/create")
+	public ResponseEntity<String> create(@RequestBody User data){
+		
+		try {
+			userRepository.save(data);
+			return new ResponseEntity<>("save [User] successful " , HttpStatus.OK);
+		} 
+		catch (Exception e) {
+			return new ResponseEntity<>("error : " + e , HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+ 	}
 	
 }
