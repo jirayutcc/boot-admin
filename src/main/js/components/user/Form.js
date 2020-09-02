8666649e7fa4bb1ea694d604f4ea5344f200ad81
Jirayut Srisawangwong <jirayut_ssww@hotmail.com>
2020-09-02 21:38:37 +0700

    [User] Form #2

--- src/main/js/components/user/Form.js
+++ src/main/js/components/user/Form.js
@@ -1,20 +1,48 @@
 import React, { Component } from "react";
 
+import userServices from "../services/User"
+
 export default class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: "",
+      email: "",
+    };
+  }
+
   render() {
     return (
       <div>
+        <h4>Create User {this.state.user}</h4>
         <div class="row">
           <div class="col-md-6 mb-3">
             <label for="username">Username</label>
-            <input type="text" class="form-control" placeholder="Username" />
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Username"
+              value={this.state.username}
+              onChange={(event) =>
+                this.setState({ username: event.target.value })
+              }
+            />
           </div>
         </div>
 
         <div class="row">
           <div class="col-md-6 mb-3">
             <label for="password">Password</label>
-            <input type="text" class="form-control" placeholder="Password" />
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={(event) =>
+                this.setState({ password: event.target.value })
+              }
+            />
           </div>
         </div>
 
@@ -25,18 +53,35 @@ export default class Form extends Component {
               type="email"
               class="form-control"
               placeholder="you@example.com"
+              value={this.state.email}
+              onChange={(event) => this.setState({ email: event.target.value })}
             />
           </div>
         </div>
 
         <div class="row">
           <div class="col-md-6 mb-3">
-            <button class="btn btn-primary btn-block" type="submit">
+            <button
+              onClick={() => this.onSave()}
+              class="btn btn-primary btn-block"
+              type="submit"
+            >
               Save
             </button>
           </div>
         </div>
       </div>
     );
+  }
+
+  onSave() {
+    const res = await userServices.create(this.state)
+
+    if (res.success) {
+      alert(res.message)
+      window.location.replace("/user/index")
+    } else {
+      alert("error : " + res.message.message)
+    }
   }
 }
