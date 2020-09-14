d9ea4734e0f3acff6482df71e4b6d4fdc216c718
Jirayut Srisawangwong <jirayut_ssww@hotmail.com>
2020-09-14 22:05:28 +0700

    [User] Edit #2

--- src/main/js/components/user/Edit.js
+++ src/main/js/components/user/Edit.js
@@ -1,24 +1,66 @@
 import React, { Component } from "react";
 
-export default class Edit extends Component {
-  render() {
-    let userId = this.props.match.params.id;
+import userServices from "../services/User";
 
+export default class Edit extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: 0,
+      username: "",
+      password: "",
+      email: "",
+    };
+  }
+
+  async componentDidMount() {
+    console.log("Mounted Edit");
+    const id = this.props.match.params.id;
+    const res = await userServices.get(id);
+    console.log(res);
+    if (res.success) {
+      console.log(res.data);
+      this.setState({
+        id: res.data.id,
+        username: res.data.username,
+        password: res.data.password,
+        email: res.data.email,
+      });
+    } else {
+      alert("Error: " + res.message);
+    }
+  }
+
+  render() {
     return (
       <div>
-        <h4>Edit customer {userId} </h4>
+        <h4>Edit User {this.state.username} </h4>
         <hr />
         <div class="row">
           <div class="col-md-6 mb-3">
             <label for="username">Username</label>
-            <input type="text" class="form-control" placeholder="Username" />
+            <input
+              type="text"
+              class="form-control"
+              value={this.state.username}
+              onChange={(value) =>
+                this.setState({ username: value.target.value })
+              }
+            />
           </div>
         </div>
 
         <div class="row">
           <div class="col-md-6 mb-3">
             <label for="password">Password</label>
-            <input type="email" class="form-control" placeholder="Password" />
+            <input
+              type="text"
+              class="form-control"
+              value={this.state.password}
+              onChange={(value) =>
+                this.setState({ password: value.target.value })
+              }
+            />
           </div>
         </div>
 
@@ -26,16 +68,21 @@ export default class Edit extends Component {
           <div class="col-md-6 mb-3">
             <label for="email">Email</label>
             <input
-              type="email"
+              type="text"
               class="form-control"
-              placeholder="you@example.com"
+              value={this.state.email}
+              onChange={(value) => this.setState({ email: value.target.value })}
             />
           </div>
         </div>
 
         <div class="row">
           <div class="col-md-6 mb-3">
-            <button class="btn btn-primary btn-block" type="submit">
+            <button
+              class="btn btn-primary btn-block"
+              onClick={() => this.onUpdate()}
+              type="submit"
+            >
               Save
             </button>
           </div>
