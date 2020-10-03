f13e2e185275b379f80af0b2f22835a375e8d3af
Jirayut Srisawangwong <jirayut_ssww@hotmail.com>
2020-10-03 22:00:17 +0700

    [User] Repository #2

--- src/main/java/com/tutofox/bootadmin/repository/UserRepository.java
+++ src/main/java/com/tutofox/bootadmin/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.tutofox.bootadmin.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,7 +9,9 @@ import com.tutofox.bootadmin.models.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 	
-	List<User> findByUsername(String name);
+	// List<User> findByUsername(String name);
 	List<User> findByUsernameLike(String name);
-	
+
+	Optional<User> findByEmail(String email);
+	Optional<User> findByUsername(String name);
 }
