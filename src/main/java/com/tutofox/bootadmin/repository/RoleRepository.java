5fc2f7c09a98a318572879794cba32497aebfead
Jirayut Srisawangwong <jirayut_ssww@hotmail.com>
2020-10-29 23:05:15 +0700

    add [Role]

---
+++ src/main/java/com/tutofox/bootadmin/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.tutofox.bootadmin.repository;
+
+import com.tutofox.bootadmin.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByRole(String role);
+
+}
