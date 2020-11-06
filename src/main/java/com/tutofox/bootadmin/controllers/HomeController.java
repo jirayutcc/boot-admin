384a4c017ea78783ebf1f18cc66dea00fdc0703c
Jirayut Srisawangwong <jirayut_ssww@hotmail.com>
2020-11-06 23:17:10 +0700

    update HomeController

--- src/main/java/com/tutofox/bootadmin/controllers/HomeController.java
+++ src/main/java/com/tutofox/bootadmin/controllers/HomeController.java
@@ -1,23 +1,78 @@
 package com.tutofox.bootadmin.controllers;
 
+import com.tutofox.bootadmin.model.User;
+import com.tutofox.bootadmin.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 public class HomeController {
-	@RequestMapping(value= {"/"}, method=RequestMethod.GET)
-	public ModelAndView home() {
-		ModelAndView model = new ModelAndView();  
-		model.setViewName("Home");
-		return model;
+
+	@Autowired
+	private UserService userService;
+
+	@GetMapping(value={"/", "/login"})
+	public ModelAndView login(){
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("login");
+		return modelAndView;
 	}
-	
-	@RequestMapping(value= {"/user"}, method=RequestMethod.GET)
-	public ModelAndView user() {
-		ModelAndView model = new ModelAndView();  
-		model.setViewName("User");
-		return model;
+
+
+	@GetMapping(value="/registration")
+	public ModelAndView registration(){
+		ModelAndView modelAndView = new ModelAndView();
+		User user = new User();
+		modelAndView.addObject("user", user);
+		modelAndView.setViewName("registration");
+		return modelAndView;
 	}
+
+	@PostMapping(value = "/registration")
+	public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
+		ModelAndView modelAndView = new ModelAndView();
+		User userExists = userService.findUserByUserName(user.getUserName());
+		if (userExists != null) {
+			bindingResult
+					.rejectValue("userName", "error.user",
+							"There is already a user registered with the user name provided");
+		}
+		if (bindingResult.hasErrors()) {
+			modelAndView.setViewName("registration");
+		} else {
+			userService.saveUser(user);
+			modelAndView.addObject("successMessage", "User has been registered successfully");
+			modelAndView.addObject("user", new User());
+			modelAndView.setViewName("registration");
+
+		}
+		return modelAndView;
+	}
+
+	@GetMapping(value={"/dashboard"})
+	public ModelAndView dashboard(){
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("dashboard");
+		return modelAndView;
+	}
+
+	@GetMapping(value="/admin/home")
+	public ModelAndView home(){
+		ModelAndView modelAndView = new ModelAndView();
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		User user = userService.findUserByUserName(auth.getName());
+		modelAndView.addObject("userName", "Welcome " + user.getUserName() + "/" + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
+		modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
+		modelAndView.setViewName("admin/home");
+		return modelAndView;
+	}
+
 }
