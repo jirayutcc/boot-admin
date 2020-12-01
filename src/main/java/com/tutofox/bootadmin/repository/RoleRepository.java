3ba95e5b4df559c8c5f4397b0d351476da277b8b
Jirayut Srisawangwong <jirayut_ssww@hotmail.com>
2020-12-01 20:42:56 +0700

    update RoleRepository

--- src/main/java/com/tutofox/bootadmin/repository/RoleRepository.java
+++ src/main/java/com/tutofox/bootadmin/repository/RoleRepository.java
@@ -1,11 +1,17 @@
 package com.tutofox.bootadmin.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.tutofox.bootadmin.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    List<Role> findAll();
+    Role findById(int role);
     Role findByRole(String role);
 
 }
