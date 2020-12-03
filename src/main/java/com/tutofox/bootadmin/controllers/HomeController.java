f3534c54eb20971b8afa22dc1623ba16eabd0ad0
Jirayut Srisawangwong <jirayut_ssww@hotmail.com>
2020-12-03 22:39:58 +0700

    update HomeController

--- src/main/java/com/tutofox/bootadmin/controllers/HomeController.java
+++ src/main/java/com/tutofox/bootadmin/controllers/HomeController.java
@@ -26,40 +26,17 @@ public class HomeController {
 		return modelAndView;
 	}
 
-
-	@GetMapping(value="/registration")
-	public ModelAndView registration(){
-		ModelAndView modelAndView = new ModelAndView();
-		User user = new User();
-		modelAndView.addObject("user", user);
-		modelAndView.setViewName("registration");
-		return modelAndView;
-	}
-
-	@PostMapping(value = "/registration")
-	public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
-		ModelAndView modelAndView = new ModelAndView();
-		User userExists = userService.findUserByUserName(user.getUserName());
-		if (userExists != null) {
-			bindingResult
-					.rejectValue("userName", "error.user",
-							"There is already a user registered with the user name provided");
-		}
-		if (bindingResult.hasErrors()) {
-			modelAndView.setViewName("registration");
-		} else {
-			userService.saveUser(user);
-			modelAndView.addObject("successMessage", "User has been registered successfully");
-			modelAndView.addObject("user", new User());
-			modelAndView.setViewName("registration");
-
-		}
-		return modelAndView;
-	}
-
 	@GetMapping(value={"/dashboard"})
 	public ModelAndView dashboard(){
 		ModelAndView modelAndView = new ModelAndView();
+
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		User user = userService.findUserByUserName(auth.getName());
+		modelAndView.addObject("userLogin", user.getUserName());
+
+		String userRole = userService.getRoleName(user.getRoles());
+		modelAndView.addObject("userLoginRole", userRole);
+
 		modelAndView.setViewName("dashboard");
 		return modelAndView;
 	}
@@ -74,5 +51,5 @@ public class HomeController {
 		modelAndView.setViewName("admin/home");
 		return modelAndView;
 	}
-
+	
 }
