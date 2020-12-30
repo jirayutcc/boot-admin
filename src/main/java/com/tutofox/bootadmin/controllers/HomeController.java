83edcee1b35a894789262321199ad490329af7ed
Jirayut Srisawangwong <jirayut_ssww@hotmail.com>
2020-12-30 12:30:43 +0700

    update HomeController

--- src/main/java/com/tutofox/bootadmin/controllers/HomeController.java
+++ src/main/java/com/tutofox/bootadmin/controllers/HomeController.java
@@ -1,7 +1,11 @@
 package com.tutofox.bootadmin.controllers;
 
 import com.tutofox.bootadmin.model.User;
+import com.tutofox.bootadmin.controllers.UserController;
 import com.tutofox.bootadmin.service.UserService;
+import com.tutofox.bootadmin.repository.UserRepository;
+import com.tutofox.bootadmin.repository.RoleRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,7 +20,17 @@ import javax.validation.Valid;
 @Controller
 public class HomeController {
 
+	private final UserRepository userRepository;
+	private final RoleRepository roleRepository;
+
+    @Autowired
+    public HomeController(UserRepository userRepository, RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+
 	@Autowired
+	private UserController userController;
 	private UserService userService;
 
 	@GetMapping(value={"/", "/login"})
@@ -29,13 +43,10 @@ public class HomeController {
 	@GetMapping(value={"/dashboard"})
 	public ModelAndView dashboard(){
 		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("userLogin", userController.getUserLogin());
+		modelAndView.addObject("userLoginRole", userController.getUserLoginRole());
 
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		User user = userService.findUserByUserName(auth.getName());
-		modelAndView.addObject("userLogin", user.getUserName());
-
-		String userRole = userService.getRoleName(user.getRoles());
-		modelAndView.addObject("userLoginRole", userRole);
+		modelAndView.addObject("users", userRepository.findAll());
 
 		modelAndView.setViewName("dashboard");
 		return modelAndView;
