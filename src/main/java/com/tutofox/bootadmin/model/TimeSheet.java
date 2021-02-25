4ec09d87e4440118916d283fa2fd0efae7da0e60
Jirayut Srisawangwong <jirayut_ssww@hotmail.com>
2021-02-25 21:30:28 +0700

    add TimeSheet

---
+++ src/main/java/com/tutofox/bootadmin/model/TimeSheet.java
@@ -0,0 +1,33 @@
+package com.tutofox.bootadmin.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "timesheet")
+public class TimeSheet {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private Integer id;
+
+	@Column(name = "date")
+	private Date date;
+
+	@Column(name = "detail")
+	private String detail;
+
+	@Column(name = "team")
+	private String team;
+}
