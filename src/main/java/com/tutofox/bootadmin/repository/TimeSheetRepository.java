682a9bc3af80c8753dc2359f21e066be4279e093
Jirayut Srisawangwong <jirayut_ssww@hotmail.com>
2021-02-26 16:27:52 +0700

    update TimeSheetRepository

--- src/main/java/com/tutofox/bootadmin/repository/TimeSheetRepository.java
+++ src/main/java/com/tutofox/bootadmin/repository/TimeSheetRepository.java
@@ -4,13 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 import com.tutofox.bootadmin.model.TimeSheet;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface TimeSheetRepository extends JpaRepository<TimeSheet, Integer> {
 
-    List<TimeSheet> findAll();
     TimeSheet findById(int timeSheet);
-
 }
