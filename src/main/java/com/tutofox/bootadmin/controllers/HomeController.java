e87d02d07135766f6fe766b9e4e64d49133c4f19
Jirayut Srisawangwong <jirayut_ssww@hotmail.com>
2021-02-27 00:06:33 +0700

    mapping timesheet controller

--- src/main/java/com/tutofox/bootadmin/controllers/HomeController.java
+++ src/main/java/com/tutofox/bootadmin/controllers/HomeController.java
@@ -2,15 +2,19 @@ package com.tutofox.bootadmin.controllers;
 
 import java.util.Optional;
 import java.util.Set;
+import java.util.List;
 import java.util.Arrays;
 import java.util.HashSet;
 
 import com.tutofox.bootadmin.model.User;
 import com.tutofox.bootadmin.model.Role;
+import com.tutofox.bootadmin.model.TimeSheet;
 import com.tutofox.bootadmin.controllers.UserController;
 import com.tutofox.bootadmin.service.UserService;
+import com.tutofox.bootadmin.service.TimeSheetService;
 import com.tutofox.bootadmin.repository.UserRepository;
 import com.tutofox.bootadmin.repository.RoleRepository;
+import com.tutofox.bootadmin.repository.TimeSheetRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -21,6 +25,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.data.domain.Page;
 
 import javax.validation.Valid;
 
@@ -31,11 +36,13 @@ public class HomeController {
 
 	private final UserRepository userRepository;
 	private final RoleRepository roleRepository;
+	private final TimeSheetRepository timeSheetRepository;
 
     @Autowired
-    public HomeController(UserRepository userRepository, RoleRepository roleRepository) {
+    public HomeController(UserRepository userRepository, RoleRepository roleRepository, TimeSheetRepository timeSheetRepository) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+		this.timeSheetRepository = timeSheetRepository;
     }
 
 	@Autowired
@@ -43,6 +50,9 @@ public class HomeController {
 	
 	@Autowired
 	private UserService userService;
+
+	@Autowired
+	private TimeSheetService timeSheetService;
 
 	@GetMapping(value={"/", "/login"})
 	public ModelAndView login(){
@@ -57,6 +67,7 @@ public class HomeController {
 		modelAndView.addObject("userLogin", userController.getUserLogin());
 		modelAndView.addObject("userLoginRole", userController.getUserLoginRole());
 
+		modelAndView.addObject("timeSheets", timeSheetRepository.findAll());
 		modelAndView.addObject("users", userRepository.findAll());
 
 		modelAndView.setViewName("dashboard");
@@ -102,15 +113,41 @@ public class HomeController {
 		return modelAndView;
     }
 
-	@GetMapping(value="/admin/home")
-	public ModelAndView home(){
+	// @GetMapping(value="/admin/home")
+	// public ModelAndView home(){
+	// 	ModelAndView modelAndView = new ModelAndView();
+	// 	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	// 	User user = userService.findUserByUserName(auth.getName());
+	// 	modelAndView.addObject("userName", "Welcome " + user.getUserName() + "/" + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
+	// 	modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
+	// 	modelAndView.setViewName("admin/home");
+	// 	return modelAndView;
+	// }
+
+	@GetMapping("/timesheet/page/{pageNum}")
+	public ModelAndView viewTimesheetPage(Model model,
+			@PathVariable(name = "pageNum") int pageNum) {
+		
 		ModelAndView modelAndView = new ModelAndView();
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		User user = userService.findUserByUserName(auth.getName());
-		modelAndView.addObject("userName", "Welcome " + user.getUserName() + "/" + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
-		modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
-		modelAndView.setViewName("admin/home");
+		modelAndView.addObject("userLogin", userController.getUserLogin());
+		modelAndView.addObject("userLoginRole", userController.getUserLoginRole());
+
+		Page<TimeSheet> page = timeSheetService.listAll(pageNum);
+		List<TimeSheet> listTimesheets = page.getContent();
+		
+		model.addAttribute("currentPage", pageNum);
+		model.addAttribute("totalPages", page.getTotalPages());
+    	model.addAttribute("totalTimeSheets", page.getTotalElements());
+        model.addAttribute("timesheets", listTimesheets);
+
+        modelAndView.setViewName("timesheet/timesheet");
+		
 		return modelAndView;
 	}
-	
+
+	@GetMapping("/timesheet")
+    public ModelAndView timeSheetList(Model model) {
+		return viewTimesheetPage(model, 1);
+    }
+
 }
